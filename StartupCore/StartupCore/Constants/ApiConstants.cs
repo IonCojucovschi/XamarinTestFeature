3d2b31079b9c32f9da84d98252fdc3185d5bea1e
Cojucovschi Ion <Cojucovschi@gmail.com>
2018-08-24 17:42:13 +0300

    Create core proj startup

---
+++ StartupCore/StartupCore/Constants/ApiConstants.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace StartupCore.Constants
+{
+    public class ApiConstants
+    {
+        public const string BaseApi = "base url";
+        public const string FirstApi = "api/users/all/";
+
+    }
+}
---
+++ StartupCore/StartupCore/Contracts/Services/Data/IAuthenticationService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using StartupCore.Models;
+
+namespace StartupCore.Contracts.Services.Data
+{
+    public interface IAuthenticationService
+    {
+        Task<AuthenticationResponse> Register(string firstName, string lastName, string email, string userName, string password);
+
+        Task<AuthenticationResponse> Authenticate(string userName, string password);
+
+        bool IsUserAuthenticated();
+    }
+}
