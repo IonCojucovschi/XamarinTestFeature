28993b93fadf4e20d3f3538a4c8cd41e0670d2ba
Cojucovschi Ion <Cojucovschi@gmail.com>
2018-08-28 17:55:29 +0300

    Get content from server example

---
+++ StartupCore/StartupCore/Services/Data/BooksDataService.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using StartupCore.Contracts.Services.Data;
+using StartupCore.Models.BooksModels;
+using StartupCore.Constants;
+using StartupCore.Contracts.Repository;
+using Akavache;
+using System.Reactive.Subjects;
+
+namespace StartupCore.Services.Data
+{
+    public class BooksDataService : BaseService, IBooksDataService
+    {
+        private readonly IGenericRepository _genericRepository;
+
+        public BooksDataService(IGenericRepository genericRepository, IBlobCache cache = null) : base(cache)
+        {
+            _genericRepository = genericRepository;
+        }
+
+
+        public Task<IEnumerable<Booklist>> GetBooklists()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Booklist>> GetBooksForCategory(IGenericRepository genericRepository, IBlobCache cache = null):base(cache)
+        {
+            UriBuilder builder = new UriBuilder(ApiConstants.BaseApi)
+            {
+                Path = ApiConstants.GetBooksForCategory
+            };
+
+            var books = await _genericRepository.GetAsync<List<Booklist>>(builder.ToString());
+
+            ///await Cache.InsertObject(CacheNameConstants.AllPies, pies, DateTimeOffset.Now.AddSeconds(20));
+
+            return books;
+        }
+
+        public async Task<IEnumerable<CategoryContent>> GetCategories()
+        {
+            UriBuilder builder = new UriBuilder(ApiConstants.BaseApi)
+            {
+                Path = ApiConstants.GetBooksCategory
+            };
+
+            var categories = await _genericRepository.GetAsync<List<CategoryContent>>(builder.ToString());
+
+            ///await Cache.InsertObject(CacheNameConstants.AllPies, pies, DateTimeOffset.Now.AddSeconds(20));
+
+            return categories;
+        }
+
+        public async Task<IEnumerable<Booklist>> IAddThisBooks()
+        {
+            UriBuilder builder = new UriBuilder(ApiConstants.BaseApi)
+            {
+                Path = ApiConstants.GetAddedBook
+            };
+
+            var books = await _genericRepository.GetAsync<List<Booklist>>(builder.ToString());
+
+            ///await Cache.InsertObject(CacheNameConstants.AllPies, pies, DateTimeOffset.Now.AddSeconds(20));
+
+            return books;
+        }
+
+        public async Task<IEnumerable<Booklist>> WishedReadBooks()
+        {
+            UriBuilder builder = new UriBuilder(ApiConstants.BaseApi)
+            {
+                Path = ApiConstants.GetWishedBook
+            };
+
+            var books = await _genericRepository.GetAsync<List<Booklist>>(builder.ToString());
+
+            ///await Cache.InsertObject(CacheNameConstants.AllPies, pies, DateTimeOffset.Now.AddSeconds(20));
+
+            return books;
+        }
+    }
+}
