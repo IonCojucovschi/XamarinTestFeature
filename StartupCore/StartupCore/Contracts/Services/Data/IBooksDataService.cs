ce6e59707d4360014af69d1c2fd36d466a5f4882
Cojucovschi Ion <Cojucovschi@gmail.com>
2018-08-28 17:56:06 +0300

    add new content to service ,,,

---
+++ StartupCore/StartupCore/Contracts/Services/Data/IBooksDataService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using StartupCore.Models.BooksModels;
+using System.Collections;
+
+namespace StartupCore.Contracts.Services.Data
+{
+    public interface IBooksDataService
+    {
+        Task<IEnumerable<Booklist>> GetBooklists();
+        Task<IEnumerable<CategoryContent>> GetCategories();
+        Task<IEnumerable<Booklist>> GetBooksForCategory();
+        Task<IEnumerable<Booklist>> WishedReadBooks();
+        Task<IEnumerable<Booklist>> IAddThisBooks();
+    }
+}
---
+++ StartupCore/StartupCore/Models/BooksModels/CategoryContent.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace StartupCore.Models.BooksModels
+{
+    public class CategoryContent : ICategoryContent
+    {
+        public string category { get; set; }
+        public string quantity { get; set; }
+        public int id { get; set; }
+    }
+}
