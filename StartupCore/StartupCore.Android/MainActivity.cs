cd8c4bc0a37cab123cff9377b08f98513abf23df
Cojucovschi Ion <Cojucovschi@gmail.com>
2018-09-03 11:10:01 +0300

    delete from global git ignore main activity

---
+++ StartupCore/StartupCore.Android/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Acr.UserDialogs;
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace StartupCore.Droid
+{
+    [Activity(Label = "StartupCore", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        internal static MainActivity Instance { get; private set; }
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            Instance = this;
+            base.OnCreate(savedInstanceState);
+
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            UserDialogs.Init(this);
+
+            var ap = new StartupCore.App();
+            LoadApplication(ap);
+        }
+    }
+}
